9854f4ad3ef29cb4ea59c29fba79734a494982e7
Osiris Peña <osiris.pena@conuco.do>
2025-06-20 11:09:19 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import CustomProvider from "@/components/theme/custom-provider";
 import Modal from "@/components/modal";
 import Footer from "@/components/footer";
 import Chatbot from "@/components/chatbot/chatbot";
+import icon from "@/public/favicon.ico";
 
 const font = FontSans({
   subsets: ["latin"],
@@ -44,13 +45,13 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon.ico", sizes: "any" },
+      // { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      // { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: icon.src, sizes: "any" },
     ],
-    apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
-    ],
+    // apple: [
+    //   { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    // ],
   },
 };
 
@@ -65,26 +66,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-      </head>
+      <head></head>
       <body
         className={`${cn(
           "min-h-screen font-sans antialiased",
